9983044d38301f258673eeeb8951aade88b6fbbb
RadvileDid <radvilesubaciute@gmail.com>
2018-02-27 10:11:13 +0000

    renaming files

--- src/app.js
+++ src/app.js
@@ -8,8 +8,8 @@ import ProtectedRoute from './components/utility/ProtectedRoute';
 
 import './scss/style.scss';
 
-import Login       from './components/auth/Login';
-import Register    from './components/auth/Register';
+import Login       from './components/auth/login';
+import Register    from './components/auth/register';
 import Navbar      from './components/Navbar';
 
 class App extends React.Component {
